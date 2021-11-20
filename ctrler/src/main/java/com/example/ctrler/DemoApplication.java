e3b3cffb6d2901af6e1f5e72b50b4b6131795c2a
Erik Vesterlund <erik.vesterlund@gmail.com>
2021-11-20 18:47:36 +0100

    joda

--- ctrler/src/main/java/com/example/ctrler/DemoApplication.java
+++ ctrler/src/main/java/com/example/ctrler/DemoApplication.java
@@ -1,13 +1,23 @@
 package com.example.ctrler;
 
+import org.joda.time.Instant;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
+    private static Instant now = Instant.now();
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @GetMapping("/hi")
+    public String hi() {
+        return String.format("hi from %s", now);
+    }
 }
